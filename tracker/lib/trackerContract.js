64e596a0cd61f7daf8d52786f5374e541f4620cb
wildlyclassyprince <wildlyclassyprince@users.noreply.github.com>
2021-10-19 12:43:16 +0400

    Initialize the ledger

--- tracker/lib/trackerContract.js
+++ tracker/lib/trackerContract.js
@@ -4,7 +4,20 @@ const shim = require('fabric-shim');
 
 const Chaincode = class {
 
-    async Init(stub){}
+    async Init(stub){
+        console.info("========== START: Initialize Ledger ==========");
+
+        const documents = [
+            {
+                id: "first-document",
+                name: "First Document"
+            }
+        ];
+
+        await stub.putState(documents[0].id, Buffer.from(JSON.stringify(documents[0])))
+
+        console.info("========== END: Initialize Ledger ==========");
+    };
 
     async Invoke(stub){}
 };
